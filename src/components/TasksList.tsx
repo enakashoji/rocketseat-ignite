ccbf51b885bd20e2ba96790229f2b8c057874be2
Carlos Eduardo Nakashoji <ext-cnakashoji@bbts.com.br>
2022-02-25 17:03:22 -0300

    Criação da funcionalidade de edição das tarefas - Rascunho

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -1,122 +1,128 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-  FlatList,
-  Image,
-  TouchableOpacity,
-  View,
-  Text,
-  StyleSheet,
-} from "react-native";
-import Icon from "react-native-vector-icons/Feather";
+	FlatList,
+	Image,
+	TouchableOpacity,
+	View,
+	Text,
+	StyleSheet,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { ItemWrapper } from "./ItemWrapper";
+import { ItemWrapper } from './ItemWrapper';
 
-import trashIcon from "../assets/icons/trash/trash.png";
+import trashIcon from '../assets/icons/trash/trash.png';
 
 export interface Task {
-  id: number;
-  title: string;
-  done: boolean;
+	id: number;
+	title: string;
+	done: boolean;
 }
 
 interface TasksListProps {
-  tasks: Task[];
-  toggleTaskDone: (id: number) => void;
-  removeTask: (id: number) => void;
+	tasks: Task[];
+	toggleTaskDone: (id: number) => void;
+	removeTask: (id: number) => void;
+	editTask: (id: number, newTaskTitle: string) => void;
 }
 
 export function TasksList({
-  tasks,
-  toggleTaskDone,
-  removeTask,
+	tasks,
+	toggleTaskDone,
+	removeTask,
+	editTask,
 }: TasksListProps) {
-  return (
-    <FlatList
-      data={tasks}
-      keyExtractor={(item) => String(item.id)}
-      contentContainerStyle={{ paddingBottom: 24 }}
-      showsVerticalScrollIndicator={false}
-      renderItem={({ item, index }) => {
-        return (
-          <ItemWrapper index={index}>
-            <View>
-              <TouchableOpacity
-                testID={`button-${index}`}
-                activeOpacity={0.7}
-                style={styles.taskButton}
-                //TODO - use onPress (toggle task) prop
-                onPress={() => toggleTaskDone(item.id)}
-              >
-                <View
-                  testID={`marker-${index}`}
-                  //TODO - use style prop
-                  style={item.done ? styles.taskMarkerDone : styles.taskMarker}
-                >
-                  {item.done && <Icon name="check" size={12} color="#FFF" />}
-                </View>
-                <Text
-                  //TODO - use style prop
-                  style={item.done ? styles.taskTextDone : styles.taskText}
-                >
-                  {item.title}
-                </Text>
-              </TouchableOpacity>
-            </View>
-
-            <TouchableOpacity
-              testID={`trash-${index}`}
-              style={{ paddingHorizontal: 24 }}
-              //TODO - use onPress (remove task) prop
-              onPress={() => removeTask(item.id)}
-            >
-              <Image source={trashIcon} />
-            </TouchableOpacity>
-          </ItemWrapper>
-        );
-      }}
-      style={{
-        marginTop: 32,
-      }}
-    />
-  );
+	return (
+		<FlatList
+			data={tasks}
+			keyExtractor={(item) => String(item.id)}
+			contentContainerStyle={{ paddingBottom: 24 }}
+			showsVerticalScrollIndicator={false}
+			renderItem={({ item, index }) => {
+				return (
+					<ItemWrapper index={index}>
+						<View>
+							<TouchableOpacity
+								testID={`button-${index}`}
+								activeOpacity={0.7}
+								style={styles.taskButton}
+								onPress={() => toggleTaskDone(item.id)}>
+								<View
+									testID={`marker-${index}`}
+									style={item.done ? styles.taskMarkerDone : styles.taskMarker}>
+									{item.done && <Icon name='check' size={12} color='#FFF' />}
+								</View>
+								<Text style={item.done ? styles.taskTextDone : styles.taskText}>
+									{item.title}
+								</Text>
+							</TouchableOpacity>
+						</View>
+						<View style={{ flexDirection: 'row' }}>
+							<TouchableOpacity
+								testID={`trash-${index}`}
+								style={{
+									paddingHorizontal: 5,
+								}}
+								onPress={() => editTask(item.id, item.title)}>
+								<Icon name='edit-3' size={20} style={styles.editButton} />
+							</TouchableOpacity>
+							<TouchableOpacity
+								testID={`trash-${index}`}
+								style={{ paddingHorizontal: 10 }}
+								onPress={() => removeTask(item.id)}>
+								<Image source={trashIcon} />
+							</TouchableOpacity>
+						</View>
+					</ItemWrapper>
+				);
+			}}
+			style={{
+				marginTop: 32,
+			}}
+		/>
+	);
 }
 
 const styles = StyleSheet.create({
-  taskButton: {
-    flex: 1,
-    paddingHorizontal: 24,
-    paddingVertical: 15,
-    marginBottom: 4,
-    borderRadius: 4,
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  taskMarker: {
-    height: 16,
-    width: 16,
-    borderRadius: 4,
-    borderWidth: 1,
-    borderColor: "#B2B2B2",
-    marginRight: 15,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  taskText: {
-    color: "#666",
-    fontFamily: "Inter-Medium",
-  },
-  taskMarkerDone: {
-    height: 16,
-    width: 16,
-    borderRadius: 4,
-    backgroundColor: "#1DB863",
-    marginRight: 15,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  taskTextDone: {
-    color: "#1DB863",
-    textDecorationLine: "line-through",
-    fontFamily: "Inter-Medium",
-  },
+	taskButton: {
+		flex: 1,
+		paddingHorizontal: 24,
+		paddingVertical: 15,
+		marginBottom: 4,
+		borderRadius: 4,
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	taskMarker: {
+		height: 16,
+		width: 16,
+		borderRadius: 4,
+		borderWidth: 1,
+		borderColor: '#B2B2B2',
+		marginRight: 15,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	taskText: {
+		color: '#666',
+		fontFamily: 'Inter-Medium',
+	},
+	taskMarkerDone: {
+		height: 16,
+		width: 16,
+		borderRadius: 4,
+		backgroundColor: '#1DB863',
+		marginRight: 15,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	taskTextDone: {
+		color: '#1DB863',
+		textDecorationLine: 'line-through',
+		fontFamily: 'Inter-Medium',
+	},
+	editButton: {
+		justifyContent: 'flex-end',
+		color: '#B2B2B2',
+	},
 });
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,57 +1,92 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useState } from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
 
-import { Header } from "../components/Header";
-import { Task, TasksList } from "../components/TasksList";
-import { TodoInput } from "../components/TodoInput";
+import { Header } from '../components/Header';
+import { Task, TasksList } from '../components/TasksList';
+import { TodoInput } from '../components/TodoInput';
+
+export interface ITask {
+	taskId: number;
+	taskNewTitle: string;
+}
 
 export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+	const [tasks, setTasks] = useState<Task[]>([]);
 
-  function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
-    const newTask: Task = {
-      id: new Date().getTime(),
-      title: newTaskTitle,
-      done: false,
-    };
-    setTasks((oldState) => [...oldState, newTask]);
-  }
+	function handleAddTask(newTaskTitle: string) {
+		if (tasks.find((task) => task.title === newTaskTitle)) {
+			Alert.alert('Task já cadastrada');
+		} else {
+			const newTask: Task = {
+				id: new Date().getTime(),
+				title: newTaskTitle,
+				done: false,
+			};
+			setTasks((oldState) => [...oldState, newTask]);
+		}
+	}
 
-  function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
-    const list: Task[] = tasks.map((task: Task) => {
-      if (task.id === id) {
-        task.done = !task.done;
-      }
-      return task;
-    });
-    setTasks(list);
-  }
+	function handleToggleTaskDone(id: number) {
+		const list: Task[] = tasks.map((task: Task) => {
+			if (task.id === id) {
+				task.done = !task.done;
+			}
+			return task;
+		});
+		setTasks(list);
+	}
 
-  function handleRemoveTask(id: number) {
-    //TODO - remove task from state
-    setTasks(tasks.filter((task) => task.id !== id));
-  }
+	function handleEditTask(id: number) {
+		const task = tasks.filter((x) => x.id === id);
+		console.log(task[0].title);
 
-  return (
-    <View style={styles.container}>
-      <Header tasksCounter={tasks.length} />
+		// const editedTaskList: Task[] = tasks.map((task) => {
+		// 	if (task.id === task.taskId) {
+		// 		task.title = task.taskNewTitle;
+		// 	}
+		// 	return task;
+		// });
+		// setTasks(editedTaskList);
+	}
 
-      <TodoInput addTask={handleAddTask} />
+	function handleRemoveTask(id: number) {
+		Alert.alert(
+			'Remover item',
+			'Tem certeza que você deseja remover esse item?',
+			[
+				{
+					text: 'Cancel',
+					onPress: () => console.log('Cancelado'),
+					style: 'cancel',
+				},
+				{
+					text: 'Remover',
+					style: 'default',
+					onPress: () => setTasks(tasks.filter((task) => task.id !== id)),
+				},
+			]
+		);
+	}
 
-      <TasksList
-        tasks={tasks}
-        toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask}
-      />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Header tasksCounter={tasks.length} />
+
+			<TodoInput addTask={handleAddTask} />
+
+			<TasksList
+				tasks={tasks}
+				toggleTaskDone={handleToggleTaskDone}
+				editTask={handleEditTask}
+				removeTask={handleRemoveTask}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#EBEBEB",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#EBEBEB',
+	},
 });
